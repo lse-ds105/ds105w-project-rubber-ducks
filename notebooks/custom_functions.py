d69e2d21f6ce71fe648fc7f41d8a7915d8a9994d
ollie-gregory <oliverjgregory@gmail.com>
2024-03-31 20:46:37 +0100

    Create 'perception' measure from NGRAMSdataset in notebooks/NB01

--- notebooks/custom_functions.py
+++ notebooks/custom_functions.py
@@ -51,7 +51,7 @@ def runQuery(query):
     timeseries = output[0]['timeseries']
     # Creating a DataFrame with year, value, and query
     years = list(range(1940, 2020))
-    df_query = pd.DataFrame({'query': query, 'Year': years, 'Appearence %': timeseries})
+    df_query = pd.DataFrame({'query': query, 'Year': years, 'Appearances': timeseries})
 
     # Displaying the final DataFrame
     return df_query
